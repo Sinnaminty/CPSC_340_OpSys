313078a10138cb48e82bec09da0f9470304c3186
sin <aidenrivera7@gmail.com>
2025-03-21 12:59:50 -0400

    Removing the head of the txt_queue linked list segfaults

--- Homework/HW4/Rivera_SJF.c
+++ Homework/HW4/Rivera_SJF.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+int ms = 0;
+
 typedef enum Priority {
   TXT, /* Highest Priority Job. */
   FP,
@@ -22,7 +24,34 @@ typedef struct Job {
 Job *txt_ready_queue = NULL;
 Job *fp_ready_queue = NULL;
 Job *mm_ready_queue = NULL;
+Job *finished_queue = NULL;
 
+////////////////////////////
+/// DEBUG FUNCTIONS
+////////////////////////////
+
+void printJob(const Job *job) {
+
+  printf("%-8s | %-12d | %-6d | %-8d | %-8d\n", job->m_name, job->m_arrivalTime,
+         job->m_length, job->m_deadline, job->m_priority);
+}
+
+// Function to print Queues
+void printQueue(const Job *queue, const char *title) {
+  printf("\n%s:\n", title);
+  printf("Job Name | Arrival Time | Length | Deadline | Priority\n");
+  printf("------------------------------------------------------\n");
+  while (queue) {
+    printJob(queue);
+    queue = queue->m_next;
+  }
+}
+void printAllQueues() {
+  printQueue(txt_ready_queue, "TXT Ready Queue");
+  printQueue(fp_ready_queue, "FP Ready Queue");
+  printQueue(mm_ready_queue, "MM Ready Queue");
+  printQueue(finished_queue, "Finished Queue");
+}
 // Function to create a new job
 Job *createJob(char *name, int arrivalTime, int length, int deadline,
                char *priority) {
@@ -93,8 +122,8 @@ void insertIntoFPReadyQueue(Job *job) {
     temp->m_previous = job;
   }
 }
-void insertIntoMMReadyQueue(Job *job) {
 
+void insertIntoMMReadyQueue(Job *job) {
   if (!mm_ready_queue) {
     mm_ready_queue = job;
     return;
@@ -116,6 +145,21 @@ void insertIntoMMReadyQueue(Job *job) {
     temp->m_previous = job;
   }
 }
+
+void insertIntoFinishedQueue(Job *job) {
+  printf("insertIntoReadyQueue\n");
+  if (!finished_queue) {
+    finished_queue = job;
+    return;
+  }
+
+  Job *temp = finished_queue;
+  while (temp->m_next) {
+    temp = temp->m_next;
+  }
+  temp->m_next = job;
+}
+
 // sorted by arrivalTime
 void insertIntoReadyQueue(Job *job) {
   switch (job->m_priority) {
@@ -130,25 +174,49 @@ void insertIntoReadyQueue(Job *job) {
     break;
   }
 }
-////////////////////////////
-/// DEBUG FUNCTIONS
-////////////////////////////
 
-void printJob(const Job *job) {
+Job *findShortestJob(Job *head) {
+  Job *shortest_job = head;
+  Job *temp = head;
+  Job *previous = NULL;
+  Job *next = NULL;
 
-  printf("%-8s | %-12d | %-6d | %-8d | %-8d\n", job->m_name, job->m_arrivalTime,
-         job->m_length, job->m_deadline, job->m_priority);
+  while (temp->m_next) {
+    temp = temp->m_next;
+    if (shortest_job->m_length > temp->m_length) {
+      shortest_job = temp;
+    }
+  }
+  // now we have the shortest Job!
+  // connect previous and next together, and insert shortestJob in
+  // finished_queue
+  previous = shortest_job->m_previous;
+  next = shortest_job->m_next;
+  shortest_job->m_next = NULL;
+  shortest_job->m_previous = NULL;
+
+  // if next is null.. that means we are at the end of the list
+  // if both are null.. that means we are the last item in this list
+
+  // if neither are null.. that means we are in the middle of the list
+  if (previous && next) {
+    previous->m_next = next;
+    next->m_previous = previous;
+
+    // if previous is null.. that means we are at the beginning of the list
+  } else if (!previous && next) {
+    *head = *head->m_next;
+  }
+
+  printf("shortest_job returned!\n");
+  printQueue(txt_ready_queue, "txt_ready_queue");
+  return shortest_job;
 }
 
-// Function to print Queues
-void printQueue(const Job *queue, const char *title) {
-  printf("\n%s:\n", title);
-  printf("Job Name | Arrival Time | Length | Deadline | Priority\n");
-  printf("------------------------------------------------------\n");
-  while (queue) {
-    printJob(queue);
-    queue = queue->m_next;
-  }
+void doJob(Job *job) {
+  // need to keep track of global time
+  // need to increment global time based on job time
+  insertIntoFinishedQueue(job);
 }
 
 // Function to load jobs from a file
@@ -171,10 +239,35 @@ void loadJobs(const char *filename) {
   fclose(file);
 }
 
+void start() {
+  // find shortest job in each queue, do it, and then put it in the finished
+  // print all queues every 500 ms
+
+  while (1) {
+    if (ms % 500 == 0) {
+      printAllQueues();
+    }
+
+    if (txt_ready_queue) {
+      doJob(findShortestJob(txt_ready_queue));
+    } else if (fp_ready_queue) {
+      doJob(findShortestJob(fp_ready_queue));
+    } else if (mm_ready_queue) {
+      doJob(findShortestJob(mm_ready_queue));
+    } else {
+      break;
+    }
+    ms++;
+  }
+}
+
 int main() {
   loadJobs("jobs.txt"); // Load jobs from file
-  printQueue(txt_ready_queue, "TXT Ready Queue");
-  printQueue(fp_ready_queue, "FP Ready Queue");
-  printQueue(mm_ready_queue, "MM Ready Queue");
+  // we start the scheduling process
+  // jobs must be done in priority, and then inserted into the finished queue
+  // when done print every 500 ms
+  start();
+  printf("Finished!\n");
+  printAllQueues();
   return 0;
 }
